4dbf0f763d9e7562642e1e8b1dc7395fe85cf647
Helen Qiong Zhang <helenqiongzhang@gmail.com>
2019-10-10 17:58:51 -0400

    added plane4marker & size4post

--- server/resolvers/Mutation.js
+++ server/resolvers/Mutation.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const { getUserId } = require('../utils');
 
 async function signup(parent, args, context, info) {
-
   const password = await bcrypt.hash(args.password, 10);
   const user = await context.prisma.createUser({ ...args, password });
   const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
@@ -15,7 +14,6 @@ async function signup(parent, args, context, info) {
 }
 
 async function login(parent, args, context, info) {
-
   const user = await context.prisma.user({ email: args.email });
 
   if (!user) {
@@ -39,20 +37,21 @@ function post(parent, args, context, info) {
   // console.log('context-----------', context)
   const userId = getUserId(context);
 
-  // post(xDistance: Float!, yDistance: Float!, zDistance: Float!, privacy: Boolean!, description: String!): Post!
+  // post(xDistance: Float!, yDistance: Float!, zDistance: Float!, privacy: Boolean!, description: String!height: Float! width: Float!): Post!
   return context.prisma.createPost({
     description: args.description,
     privacy: args.privacy,
     xDistance: args.xDistance,
     yDistance: args.yDistance,
     zDistance: args.zDistance,
+    height: args.height,
+    width: args.width,
     marker: { connect: { id: 'ck1l5gdahczar0b40frop08ju' } },
     postPostedBy: { connect: { id: userId } },
-
   });
 }
 
-async function comment(parent, args, context, info) {
+function comment(parent, args, context, info) {
   const userId = getUserId(context);
 
   return context.prisma.createComment({
@@ -62,20 +61,21 @@ async function comment(parent, args, context, info) {
   });
 }
 
-async function addMarker(parent, args, context, info) {
+function addMarker(parent, args, context, info) {
   const userId = getUserId(context);
-  // addMarker(description: String!, imageUrl: String!, longitude: Float!, latitude: Float!, height: Float!): Marker!
+  // addMarker(description: String!, imageUrl: String!, longitude: Float!, latitude: Float!, height: Float! plane: String!): Marker!
   return context.prisma.createMarker({
     description: args.description,
     imageUrl: args.imageUrl,
     longitude: args.longitude,
     latitude: args.latitude,
     height: args.height,
+    plane: args.plane,
     markerPostedBy: { connect: { id: userId } },
   });
 }
 
-async function editPost(parent, args, context, info) {
+function editPost(parent, args, context, info) {
   const userId = getUserId(context);
   // editPost(id: ID!, description: String!): Post!
   return context.prisma.updatePost({
@@ -84,19 +84,19 @@ async function editPost(parent, args, context, info) {
     },
     where: {
       id: args.id,
-    }
+    },
   });
 }
 
-async function deletePost(parent, args, context, info) {
+function deletePost(parent, args, context, info) {
   const userId = getUserId(context);
   // editPost(id: ID!, description: String!): Post!
   return context.prisma.deletePost({
-      id: args.id,
+    id: args.id,
   });
 }
 
-async function editComment(parent, args, context, info) {
+function editComment(parent, args, context, info) {
   const userId = getUserId(context);
   // editPost(id: ID!, description: String!): Post!
   return context.prisma.updateComment({
@@ -105,15 +105,15 @@ async function editComment(parent, args, context, info) {
     },
     where: {
       id: args.id,
-    }
+    },
   });
 }
 
-async function deleteComment(parent, args, context, info) {
+function deleteComment(parent, args, context, info) {
   const userId = getUserId(context);
   // editPost(id: ID!, description: String!): Post!
   return context.prisma.deleteComment({
-      id: args.id,
+    id: args.id,
   });
 }
 
@@ -126,5 +126,5 @@ module.exports = {
   editPost,
   deletePost,
   editComment,
-  deleteComment
+  deleteComment,
 };
--- server/resolvers/Query.js
+++ server/resolvers/Query.js
@@ -1,11 +1,9 @@
-
 async function feed(parent, args, context, info) {
-  console.log('entered--------------------')
-  const posts = await context.prisma.posts()
-  return posts
+  console.log('entered--------------------');
+  const posts = await context.prisma.posts();
+  return posts;
 }
 
 module.exports = {
   feed,
-}
-
+};
