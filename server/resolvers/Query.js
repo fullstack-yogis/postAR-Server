8f97e795ad6ade32f6741665eef315e13390706e
Rushil Shakya <rushil.shakya@gmail.com>
2019-10-13 15:47:59 -0400

    adding query for single post with id as parameter

--- server/resolvers/Query.js
+++ server/resolvers/Query.js
@@ -3,6 +3,12 @@ async function feed(parent, args, context, info) {
   return posts;
 }
 
+async function post(parent, args, context, info) {
+  const post = await context.prisma.post({ id: args.id });
+  return post;
+}
+
 module.exports = {
   feed,
+  post,
 };
